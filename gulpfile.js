57b7337a4a602eb60aadb068ed86c4d28fdd831a
_tango <shurinskiy@mail.ru>
2023-04-24 11:27:39 +0600

    add header & hero

--- gulpfile.js
+++ gulpfile.js
@@ -137,8 +137,8 @@ function icons() {
 	.pipe($.svgSymbolView({
 		name: 'icons-sprite',
 		monochrome: {
-			blue: '#638bf5',
-			dark: '#46557b',
+			light: '#ffe1b5',
+			black: '#2d2d2d',
 			white: '#ffffff'
 		}
 	}))
