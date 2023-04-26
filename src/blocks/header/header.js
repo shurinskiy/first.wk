111e03d41b50cfd7f6fcc46252bd671f5f6bacb3
_tango <shurinskiy@mail.ru>
2023-04-26 10:36:20 +0600

    homepage ready

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,5 +1,6 @@
 import scrollLock from 'scroll-lock';
 import { menuToggle } from "../../js/libs/menuToggle";
+import { scrollToId } from "../../js/libs/helpers";
 
 (() => {
 	const header = document.querySelector('.header');
@@ -7,17 +8,17 @@ import { menuToggle } from "../../js/libs/menuToggle";
 	
 	const vh = window.visualViewport.height * 0.01;
 	const navi = header.querySelector('.header__navi');
-	const toggles = header.querySelectorAll('.header__navi-toggle, .h-navi__close');
+	const toggles = header.querySelectorAll('.header__navi-toggle, .navi-h__close');
 	
 	const menu = menuToggle(navi, toggles, {
 		scrollLock: scrollLock,
 		omitToClose: '.modal',
 		class: 'opened'
 	});
-
+	
 	// открытие и закрытие меню, свайпом на мобильных устройствах
 	navi.addEventListener('swiped-left', (e) => menu.menuClose(e));
-
+	
 	// решение проблемы 100vh для меню на мобильных устройствах
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 	
@@ -26,5 +27,6 @@ import { menuToggle } from "../../js/libs/menuToggle";
 			document.documentElement.style.setProperty('--vh', `${e.target.height * 0.01}px`);
 		});
 	});
-
+	
+	scrollToId(document.querySelectorAll('a[href^="#"]'));
 })();
